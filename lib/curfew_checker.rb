50de3f1ecf36e416a1e30fee0b86f6660aedddee
Isabella Duran <2020ikd7@gmail.com>
2018-06-19 19:28:21 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -25,12 +25,19 @@ end
 complex_curfew_checker(9)
 
 def deluxe_curfew_checker(time)
-  # code goes here
+    
+    if time == 11
+    puts "your late, grounded"
+    elsif time == 12
+    puts "you're one hour late"
+    elsif time == 10 
+    puts " one hour left be home soon"
+  else time == 9 
+    puts "2 hours left don't be late"
+  end 
 end
 
 def platinum_curfew_checker(current_time, curfew_time)
   # code goes here
 end
 
-
-simpl
